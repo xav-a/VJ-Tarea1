92047b1d7e3e936811ef15abe07b87e1b6796bb7
xav-a <xav_andrade@hotmail.com>
2019-06-10 23:51:21 -0500

    [On branch: dev] Created Prefab to deal with animation selection, ItemCreate script added to instantiate Prefab instances in scene

---
+++ Assets/Scripts/ItemCreate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCreate : MonoBehaviour
+{
+    //Camera camera;
+
+    public GameObject itemPrefab;
+    public GameObject tileMap;
+    public int rayDistance = 10;
+
+    void Start()
+    {
+        //worldCamera = GetComponent<Camera>();
+    }
+
+
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log($"click happens at {clickPosition.x}, {clickPosition.y}");
+            clickPosition = new Vector3(clickPosition.x, clickPosition.y, 0);
+            GameObject copy = Instantiate(
+                        itemPrefab,
+                        clickPosition,
+                        Quaternion.identity);
+            copy.transform.parent = tileMap.transform;
+        }
+    }
+
+
+}
